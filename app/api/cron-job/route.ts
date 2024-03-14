772ef1881c48af16d5c4280fff4501f044537fd9
Sevn_ <71951344+Bethel-nz@users.noreply.github.com>
2024-03-14 14:11:55 +0000

    feat: added smtp

--- app/api/cron-job/route.ts
+++ app/api/cron-job/route.ts
@@ -1,42 +1,44 @@
-import { NextResponse } from "next/server";
-import prisma from "@/prisma/client";
-import nodemailer from "nodemailer";
+import { NextResponse } from 'next/server';
+import prisma from '@/prisma/client';
+import nodemailer from 'nodemailer';
 
 export const maxDuration = 10;
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export async function GET(request: Request) {
-  try {
-    const users = await prisma.users.findMany();
+	try {
+		const users = await prisma.users.findMany();
 
-    const quote_count = await prisma.quotes.count();
-    const quote = await prisma.quotes.findFirst({
-      skip: Math.floor(Math.random() * quote_count),
-    });
+		const quote_count = await prisma.quotes.count();
+		const quote = await prisma.quotes.findFirst({
+			skip: Math.floor(Math.random() * quote_count),
+		});
 
-    if (!quote) {
-      throw new Error("Error fetching quote");
-    }
+		if (!quote) {
+			throw new Error('Error fetching quote');
+		}
 
-    if (!users) throw new Error("No users");
+		if (!users) throw new Error('No users');
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "quotebox@gmail.com",
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+		const transporter = nodemailer.createTransport({
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth: {
+				user: 'aautominner@gmail.com',
+				pass: process.env.EMAIL_PASSWORD,
+			},
+		});
 
-    const emailPromises = users.map((user) => {
-      const unsubscribeLink = `https://your-domain.com/api/unsubscribe?token=${user.token}`;
+		const emailPromises = users.map((user) => {
+			const unsubscribeLink = `https://your-domain.com/api/unsubscribe?token=${user.token}`;
 
-      const mailOptions = {
-        from: "quotebox@gmail.com",
-        to: user.email,
-        subject: "Today's Quote",
-        text: `
+			const mailOptions = {
+				from: 'quotebox@gmail.com',
+				to: user.email,
+				subject: "Today's Quote",
+				text: `
           <div>
           <h2>Quote of the day!</h2>
           <p>${quote.quote}</p>
@@ -47,28 +49,28 @@ export async function GET(request: Request) {
           <p>Click to <a href="${unsubscribeLink}">Unsubscribe</a></p>
           </div>
         `,
-      };
-      return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error(`Error sending email to ${user.email}:`, error);
-            reject(error);
-          } else {
-            console.log(`Email sent to ${user.email}:`, info.response);
-            resolve(info);
-          }
-        });
-      });
-    });
+			};
+			return new Promise((resolve, reject) => {
+				transporter.sendMail(mailOptions, (error, info) => {
+					if (error) {
+						console.error(`Error sending email to ${user.email}:`, error);
+						reject(error);
+					} else {
+						console.log(`Email sent to ${user.email}:`, info.response);
+						resolve(info);
+					}
+				});
+			});
+		});
 
-    await Promise.all(emailPromises);
+		await Promise.all(emailPromises);
 
-    return NextResponse.json({
-      message: "Ok",
-      data: "successfully sent mails",
-    });
-  } catch (error: any) {
-    console.log(error);
-    throw new Error(`Failed to get all users: ${error.message}`);
-  }
+		return NextResponse.json({
+			message: 'Ok',
+			data: 'successfully sent mails',
+		});
+	} catch (error: any) {
+		console.log(error);
+		throw new Error(`Failed to get all users: ${error.message}`);
+	}
 }
