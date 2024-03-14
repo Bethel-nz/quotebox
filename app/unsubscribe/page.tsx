8a0212bdb6585c3915b5f445ddcc7afac6566ef4
Sevn_ <71951344+Bethel-nz@users.noreply.github.com>
2024-03-14 13:34:25 +0000

    fix: added search param token

--- app/unsubscribe/page.tsx
+++ app/unsubscribe/page.tsx
@@ -1,9 +1,16 @@
-import Unsubscribe from "@/components/Unsubscribe";
+import Unsubscribe from '@/components/Unsubscribe';
 
-export default function page() {
-  return (
-    <div className="bg-white min-h-screen w-full flex flex-col items-center justify-center space-y-6 p-6">
-      <Unsubscribe />
-    </div>
-  );
+type Props = {
+	searchParams: {
+		token: string;
+	};
+};
+
+export default function page({ searchParams }: Props) {
+	const { token } = searchParams;
+	return (
+		<div className='bg-white min-h-screen w-full flex flex-col items-center justify-center space-y-6 p-6'>
+			<Unsubscribe token={token} />
+		</div>
+	);
 }
--- components/Unsubscribe.tsx
+++ components/Unsubscribe.tsx
@@ -1,59 +1,57 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { useToast } from "@/components/ui/use-toast";
-import { useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { removeUserFromMailList } from "@/lib/actions";
-import { Loader } from "lucide-react";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
+import { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { removeUserFromMailList } from '@/lib/actions';
+import { Loader } from 'lucide-react';
+import Link from 'next/link';
 
-const Unsubscribe = () => {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const { toast } = useToast();
-  const [loading, setLoading] = useState(false);
+const Unsubscribe = ({ token }: { token: string }) => {
+	const { toast } = useToast();
+	const [loading, setLoading] = useState(false);
 
-  const handleUnsubscribe = async () => {
-    setLoading(true);
-    try {
-      await removeUserFromMailList(token || "");
-      toast({
-        variant: "successful",
-        title: "Successful.",
-        description: "You have been removed from the mail list.",
-      });
-    } catch (error) {
-      console.log(error);
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: "There was a problem with your request.",
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <div className="flex flex-col space-y-6 items-center justify-center min-w-fit">
-      <p className="font-bold text-base">
-        Are you sure you want to unsubscribe?
-      </p>
+	const handleUnsubscribe = async () => {
+		setLoading(true);
+		try {
+			await removeUserFromMailList(token || '');
+			toast({
+				variant: 'successful',
+				title: 'Successful.',
+				description: 'You have been removed from the mail list.',
+			});
+		} catch (error) {
+			console.log(error);
+			toast({
+				variant: 'destructive',
+				title: 'Uh oh! Something went wrong.',
+				description: 'There was a problem with your request.',
+			});
+		} finally {
+			setLoading(false);
+		}
+	};
+	return (
+		<div className='flex flex-col space-y-6 items-center justify-center min-w-fit'>
+			<p className='font-bold text-base'>
+				Are you sure you want to unsubscribe?
+			</p>
 
-      <Button
-        type="submit"
-        onClick={handleUnsubscribe}
-        className="font-semibold w-full"
-      >
-        {loading && <Loader color="black" className="animate-spin mr-2" />}
-        {loading ? "Unsubscribing..." : "Yes, Unsubscribe me"}
-      </Button>
+			<Button
+				type='submit'
+				onClick={handleUnsubscribe}
+				className='font-semibold w-full'
+			>
+				{loading && <Loader color='black' className='animate-spin mr-2' />}
+				{loading ? 'Unsubscribing...' : 'Yes, Unsubscribe me'}
+			</Button>
 
-      <Button variant="outline" className="w-full">
-        <Link href="/">No, Keep me subscribed</Link>
-      </Button>
-    </div>
-  );
+			<Button variant='outline' className='w-full'>
+				<Link href='/'>No, Keep me subscribed</Link>
+			</Button>
+		</div>
+	);
 };
 
 export default Unsubscribe;
